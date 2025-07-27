d9b81164587467c9145ff54061777dcfbba82e94
unknown <tridianayunita@gmail.com>
2025-07-27 10:30:16 +0700

    feat:react deploy

---
+++ react-deploy/src/App.jsx
@@ -0,0 +1,17 @@
+import HomePage from "./pages/HomePage"
+import LoginPage from "./pages/LoginPage"
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element= {<HomePage />}></Route>
+        <Route path='/login' element= {<LoginPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
